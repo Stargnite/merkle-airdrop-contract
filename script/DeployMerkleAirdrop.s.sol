b4f49912c5e4066e4e666470a0daa201c0822b15
shobam <tobyolamide22@gmail.com>
2026-01-13 22:42:15 +0100

    added makefile and interactions file

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {BagelToken} from "../src/BagelToken.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
-import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract DeployMerkleAirdrop is Script {
     BagelToken token;
